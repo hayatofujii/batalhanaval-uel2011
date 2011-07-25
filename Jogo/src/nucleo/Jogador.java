46e5622bbda901d0db293d37c706497716b20358
andredg91 <andredg91@gmail.com>
2011-07-25 19:36:14 +0000

    inicial

---
+++ Jogo/src/nucleo/Jogador.java
@@ -0,0 +1,53 @@
+package nucleo;
+
+public class Jogador {
+
+    private String nome;
+    private byte pontos;
+    private Grid campo;
+
+    public Jogador (String _nome)
+    {
+        this.nome = _nome;
+        this.pontos = 0;
+        this.campo = new Grid();
+    }
+    
+    public int pegaCampo(int posX, int posY)
+    {
+        return campo.abreCampo(posX, posY);
+    }
+    
+    public void refazCampo()
+    {
+        this.campo.limpa();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public byte getPontos() {
+        return pontos;
+    }
+    
+    public void fazJogada (Jogador oponente, int posX, int posY)
+    {
+        int rodadaSeguida = 0;
+        boolean sair = false;
+        
+        do
+        {
+            if (oponente.pegaCampo(posX, posY) > 0)
+            {
+                rodadaSeguida++;
+                pontos += rodadaSeguida * 10;
+            }
+            else
+                sair = true;
+
+        } while (!sair);
+        
+        
+    }
+}
