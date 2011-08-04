387ac3057a46571073a8f1297de9a6c87fda408f
oliveira.ph17 <oliveira.ph17@gmail.com>
2011-08-04 19:46:30 +0000

    Mais algumas alterações no ConfigGrid.

--- src/janelas/ConfigGrid.java
+++ src/janelas/ConfigGrid.java
@@ -1,14 +1,8 @@
 package janelas;
 
-import componentesUI.Chat;
-import componentesUI.Grid;
-import java.awt.Dimension;
 import javax.swing.JPanel;
 
 public class ConfigGrid extends JPanel {
-    Chat chat;
-    Grid grid;
-
     public ConfigGrid() {
         
     }
