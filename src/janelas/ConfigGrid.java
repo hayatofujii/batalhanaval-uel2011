b7c46a5f170804e147a872cca8132daaf58ebaba
oliveira.ph17 <oliveira.ph17@gmail.com>
2011-08-04 19:45:13 +0000

    Algumas alterações no ConfigGrid.

--- src/janelas/ConfigGrid.java
+++ src/janelas/ConfigGrid.java
@@ -10,19 +10,6 @@ public class ConfigGrid extends JPanel {
     Grid grid;
 
     public ConfigGrid() {
-        grid = new Grid();
-        chat = new Chat("Paulo Henrique");
         
-        setLayout(null);
-        setPreferredSize(new Dimension(800, 600));
-        
-        grid.setPreferredSize(new Dimension(350, 350));
-        Dimension tamGrid = grid.getPreferredSize();
-        grid.setBounds(50, 50, tamGrid.width, tamGrid.height);
-        add(grid);
-        
-        Dimension tamChat = chat.getPreferredSize();
-        chat.setBounds(50, tamGrid.height + 58, tamChat.width, tamChat.height);
-        add(chat);
     }
 }
