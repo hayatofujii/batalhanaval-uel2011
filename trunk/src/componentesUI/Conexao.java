cd984142fdad6a6075f8ab3609c2fdf0024b69a5
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@24d9eb36-21fd-3d85-d95d-9799f4f8e406>
2011-08-04 17:16:06 +0000

    Algumas alterações no form de conexão.

--- trunk/src/componentesUI/Conexao.java
+++ trunk/src/componentesUI/Conexao.java
@@ -5,6 +5,8 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -20,17 +22,17 @@ public class Conexao extends JPanel {
     private JTextField campoIP;
     private JLabel labelPorta;
     private JTextField campoPorta;
-    private JLabel status;
-    private JButton conectar;
+    private JLabel labelStatus;
+    private JButton botConectar;
     
-    private JLabel labelNome2;
-    private JTextField campoNome2;
-    private JLabel labelIP2;
-    private JTextField campoIP2;
-    private JLabel labelPorta2;
-    private JTextField campoPorta2;
-    private JLabel status2;
-    private JButton conectar2;
+    private JLabel labelNomeSv;
+    private JTextField campoNomeSv;
+    private JLabel labelIPSv;
+    private JTextField campoIPSv;
+    private JLabel labelPortaSv;
+    private JTextField campoPortaSv;
+    private JLabel labelStatusSv;
+    private JButton botConectarSv;
     
     public Conexao() {
         abas = new JTabbedPane();
@@ -89,18 +91,19 @@ public class Conexao extends JPanel {
         
         labelNome.setLabelFor(campoNome);
         
-        status = new JLabel("Status:");
-        status.setPreferredSize(new Dimension(40, 17));
-        Dimension tamStatus = status.getPreferredSize();
-        status.setBounds(8, tamLabelIP.height + tamLabelPorta.height + tamLabelNome.height + 32, tamStatus.width, tamStatus.height);
-        formCliente.add(status);
+        labelStatus = new JLabel("Status:");
+        labelStatus.setPreferredSize(new Dimension(40, 17));
+        Dimension tamStatus = labelStatus.getPreferredSize();
+        labelStatus.setBounds(8, 102, tamStatus.width, tamStatus.height);
+        formCliente.add(labelStatus);
         
-        conectar = new JButton("Conectar");
-        conectar.setPreferredSize(new Dimension(60, 17));
-        Dimension tamConectar = conectar.getPreferredSize();
-        conectar.setMargin(new Insets(0, 0, 0, 0));
-        conectar.setBounds(132, 102, tamConectar.width, tamConectar.height);
-        formCliente.add(conectar);
+        botConectar = new JButton("Conectar");
+        botConectar.setPreferredSize(new Dimension(60, 17));
+        Dimension tamConectar = botConectar.getPreferredSize();
+        botConectar.setMargin(new Insets(0, 0, 0, 0));
+        botConectar.setBounds(70, tamLabelIP.height + tamLabelPorta.height + tamLabelNome.height + 32, tamConectar.width, tamConectar.height);
+        botConectar.addActionListener(new eventoBotConectar());
+        formCliente.add(botConectar);
         
         abas.addTab("Cliente", formCliente);
     }
@@ -110,63 +113,78 @@ public class Conexao extends JPanel {
         formServidor = new JPanel();
         formServidor.setLayout(null);
         
-        labelIP2 = new JLabel("IP:");
-        labelIP2.setPreferredSize(new Dimension(40, 17));
-        Dimension tamLabelIP2 = labelIP2.getPreferredSize();
-        labelIP2.setBounds(8, 8, tamLabelIP2.width, tamLabelIP2.height);
-        formServidor.add(labelIP2);
+        labelIPSv = new JLabel("IP:");
+        labelIPSv.setPreferredSize(new Dimension(40, 17));
+        Dimension tamLabelIPSv = labelIPSv.getPreferredSize();
+        labelIPSv.setBounds(8, 8, tamLabelIPSv.width, tamLabelIPSv.height);
+        formServidor.add(labelIPSv);
         
-        campoIP2 = new JTextField();
-        campoIP2.setPreferredSize(new Dimension(98, 17));
-        Dimension tamCampoIP2 = campoIP2.getPreferredSize();
-        campoIP2.setBounds(52, 8, tamCampoIP2.width, tamCampoIP2.height);
-        campoIP2.setText("255.255.255.255");
-        campoIP2.setEditable(false);
-        formServidor.add(campoIP2);
+        campoIPSv = new JTextField();
+        campoIPSv.setPreferredSize(new Dimension(98, 17));
+        Dimension tamCampoIPSv = campoIPSv.getPreferredSize();
+        campoIPSv.setBounds(52, 8, tamCampoIPSv.width, tamCampoIPSv.height);
+        campoIPSv.setText("255.255.255.255");
+        campoIPSv.setEditable(false);
+        formServidor.add(campoIPSv);
         
-        labelIP2.setLabelFor(campoIP2);
+        labelIPSv.setLabelFor(campoIPSv);
         
-        labelPorta2 = new JLabel("Porta:");
-        labelPorta2.setPreferredSize(new Dimension(40, 17));
-        Dimension tamLabelPorta2 = labelPorta2.getPreferredSize();
-        labelPorta2.setBounds(8, tamLabelIP2.height + 16, tamLabelPorta2.width, tamLabelPorta2.height);
-        formServidor.add(labelPorta2);
+        labelPortaSv = new JLabel("Porta:");
+        labelPortaSv.setPreferredSize(new Dimension(40, 17));
+        Dimension tamLabelPortaSv = labelPortaSv.getPreferredSize();
+        labelPortaSv.setBounds(8, tamLabelIPSv.height + 16, tamLabelPortaSv.width, tamLabelPortaSv.height);
+        formServidor.add(labelPortaSv);
         
-        campoPorta2 = new JTextField();
-        campoPorta2.setPreferredSize(new Dimension(40, 17));
-        Dimension tamCampoPorta2 = campoPorta2.getPreferredSize();
-        campoPorta2.setBounds(52, tamLabelIP2.height + 16, tamCampoPorta2.width, tamCampoPorta2.height);
-        formServidor.add(campoPorta2);
+        campoPortaSv = new JTextField();
+        campoPortaSv.setPreferredSize(new Dimension(40, 17));
+        Dimension tamCampoPortaSv = campoPortaSv.getPreferredSize();
+        campoPortaSv.setBounds(52, tamLabelIPSv.height + 16, tamCampoPortaSv.width, tamCampoPortaSv.height);
+        formServidor.add(campoPortaSv);
         
-        labelPorta2.setLabelFor(campoPorta2);
+        labelPortaSv.setLabelFor(campoPortaSv);
         
-        labelNome2 = new JLabel("Nome:");
-        labelNome2.setPreferredSize(new Dimension(40, 17));
-        Dimension tamLabelNome2 = labelNome2.getPreferredSize();
-        labelNome2.setBounds(8, tamLabelIP2.height + tamLabelPorta2.height + 24, tamLabelNome2.width, tamLabelNome2.height);
-        formServidor.add(labelNome2);
+        labelNomeSv = new JLabel("Nome:");
+        labelNomeSv.setPreferredSize(new Dimension(40, 17));
+        Dimension tamLabelNomeSv = labelNomeSv.getPreferredSize();
+        labelNomeSv.setBounds(8, tamLabelIPSv.height + tamLabelPortaSv.height + 24, tamLabelNomeSv.width, tamLabelNomeSv.height);
+        formServidor.add(labelNomeSv);
         
-        campoNome2 = new JTextField();
-        campoNome2.setPreferredSize(new Dimension(98, 17));
-        Dimension tamCampoNome2 = campoNome2.getPreferredSize();
-        campoNome2.setBounds(52, tamLabelIP2.height + tamLabelPorta2.height + 24, tamCampoNome2.width, tamCampoNome2.height);
-        formServidor.add(campoNome2);
+        campoNomeSv = new JTextField();
+        campoNomeSv.setPreferredSize(new Dimension(98, 17));
+        Dimension tamCampoNomeSv = campoNomeSv.getPreferredSize();
+        campoNomeSv.setBounds(52, tamLabelIPSv.height + tamLabelPortaSv.height + 24, tamCampoNomeSv.width, tamCampoNomeSv.height);
+        formServidor.add(campoNomeSv);
         
-        labelNome2.setLabelFor(campoNome2);
+        labelNomeSv.setLabelFor(campoNomeSv);
         
-        status2 = new JLabel("Status:");
-        status2.setPreferredSize(new Dimension(40, 17));
-        Dimension tamStatus2 = status2.getPreferredSize();
-        status2.setBounds(8, tamLabelIP2.height + tamLabelPorta2.height + tamLabelNome2.height + 32, tamStatus2.width, tamStatus2.height);
-        formServidor.add(status2);
+        labelStatusSv = new JLabel("Status:");
+        labelStatusSv.setPreferredSize(new Dimension(40, 17));
+        Dimension tamStatusSv = labelStatusSv.getPreferredSize();
+        labelStatusSv.setBounds(8, 102, tamStatusSv.width, tamStatusSv.height);
+        formServidor.add(labelStatusSv);
         
-        conectar2 = new JButton("Conectar");
-        conectar2.setPreferredSize(new Dimension(60, 17));
-        Dimension tamConectar2 = conectar2.getPreferredSize();
-        conectar2.setMargin(new Insets(0, 0, 0, 0));
-        conectar2.setBounds(132, 102, tamConectar2.width, tamConectar2.height);
-        formServidor.add(conectar2);
+        botConectarSv = new JButton("Iniciar");
+        botConectarSv.setPreferredSize(new Dimension(60, 17));
+        Dimension tamConectarSv = botConectarSv.getPreferredSize();
+        botConectarSv.setMargin(new Insets(0, 0, 0, 0));
+        botConectarSv.setBounds(70, tamLabelIPSv.height + tamLabelPortaSv.height + tamLabelNomeSv.height + 32, tamConectarSv.width, tamConectarSv.height);
+        botConectarSv.addActionListener(new eventoBotConectarSv());
+        formServidor.add(botConectarSv);
         
         abas.addTab("Servidor", formServidor);
+    }
+    
+    private class eventoBotConectar implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            
+        }
+    }
+    
+    private class eventoBotConectarSv implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            
+        }
     }
 }
