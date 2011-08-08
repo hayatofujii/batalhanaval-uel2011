08e0401e39db612676782f838bc223719509f8f3
oliveira.ph17@gmail.com <oliveira.ph17@gmail.com@24d9eb36-21fd-3d85-d95d-9799f4f8e406>
2011-08-08 20:46:38 +0000

    Parte de conexão com poucas alterações (sem funcionar ainda).

--- trunk/src/nucleo/Conexao.java
+++ trunk/src/nucleo/Conexao.java
@@ -1,29 +1,37 @@
 package nucleo;
 
+import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- * Singleton de gerenciador de conexão.
- * @author hayato
- */
 public class Conexao {
-
     private static Conexao conexao;
     private Socket soqueteCliente;
     private ServerSocket soqueteServer;
-    
     private int portaConexao;
+    private String entrada;
+    private String saida;
 
     private Conexao() {
         // whatever you want to do
     }
 
     public static Conexao getConexao() {
-        if (conexao == null) {
+        if(conexao == null) {
             conexao = new Conexao();
         }
-
         return conexao;
+    }
+    
+    public class EventoRecebeMensagem implements Runnable {
+        private BufferedReader reader;
+        
+        public EventoRecebeMensagem() {
+            //reader = 
+        }
+        
+        public void run() {
+            //while(entrada = )
+        }
     }
 }
