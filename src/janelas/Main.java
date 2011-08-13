74b57dae6b123e72ce78dc00d65950f234771914
oliveira.ph17 <oliveira.ph17@gmail.com>
2011-08-13 18:56:26 +0000

    Transição da tela de conexão para a do grid (com bugs).

--- src/janelas/Main.java
+++ src/janelas/Main.java
@@ -1,4 +1,5 @@
 package janelas;
+import java.awt.Color;
 import nucleo.Jogador;
 
 import javax.swing.SwingUtilities;
@@ -8,6 +9,8 @@ import java.awt.Dimension;
 public class Main {
     private Jogador jogador;
     private static JFrame janela;
+    private static ConfigConexao confConexao;
+    private static ConfigGrid confGrid;
     
     private static final int MAX_LARGURA = 800;
     private static final int MAX_ALTURA = 600;
@@ -29,18 +32,38 @@ public class Main {
         return janela;
     }
     
+    public static ConfigConexao getConfigConexao() {
+        if(confConexao == null)
+            confConexao = new ConfigConexao();
+        return confConexao;
+    }
+    
+    public static ConfigGrid getConfigGrid() {
+        if(confGrid == null)
+            confGrid = new ConfigGrid();
+        return confGrid;
+    }
+    
+    public static void atualizaPainelConfigGrid() {
+        janela.remove(confConexao);
+        confGrid = getConfigGrid();
+        confGrid.setOpaque(true);
+        janela.add(confGrid);
+        confGrid.revalidate();
+    }
+    
     private static void mostrar() {
         // frame
-        getJFrame();
+        janela = getJFrame();
         janela.setPreferredSize(new Dimension(MAX_LARGURA, MAX_ALTURA));
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setResizable(false);
         
-        // instancia uma janela ConfigGrid
-        ConfigConexao confConexao = new ConfigConexao();
+        // instancia um painel ConfigConexao
+        confConexao = getConfigConexao();
         confConexao.setOpaque(true);
         
-        // seta a janela para mostrar somente o Config
+        // adiciona o painel à janela
         janela.add(confConexao);
         
         // mostrar frame
